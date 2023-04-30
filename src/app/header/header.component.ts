eeac1cf6e4c93efcb3ae807878e252a4d77dc70d
Wilhelm Teicke <wilhelm.teicke@googlemail.com>
2023-04-30 22:29:49 +0200

    header menu started, fist page layout finished

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,15 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
-
+export class HeaderComponent  {
+ 
   showMenu = false;
 
   openMenu() {
     this.showMenu = true;
+    console.log("menu open")
   }
 
   closeMenu() {
     this.showMenu = false;
+    console.log("menu close")
   }
+
+
 }
