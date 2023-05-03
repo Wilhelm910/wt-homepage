045c05811dce446b7d6072a60fe50e59372f5de1
Wilhelm Teicke <wilhelm.teicke@googlemail.com>
2023-05-03 07:41:14 +0200

    dekstop version update

--- src/app/aboutme/aboutme.component.ts
+++ src/app/aboutme/aboutme.component.ts
@@ -6,15 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./aboutme.component.scss']
 })
 export class AboutmeComponent {
-  movedDiag = false;
-  movedHead = false;
-  hideDiag = false;
 
-  hovered() {
-    this.movedDiag = true;
-    this.movedHead = true;
-    setTimeout(() => {
-      this.hideDiag = true;
-    }, 800);
-  }
 }
