fc96664d29dded1e4910965a085198360d7f8a5b
Wilhelm Teicke <wilhelm.teicke@googlemail.com>
2023-05-10 18:27:00 +0200

    aos added, old animations removed

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as data from 'src/assets/json/projects.json';
+import AOS from "aos";
 
 @Component({
   selector: 'app-portfolio',
@@ -11,7 +12,7 @@ export class PortfolioComponent implements OnInit {
   projectData: any = data.default;
  
   ngOnInit(): void {
- 
+    AOS.init();
   }
 
   hovered() {
